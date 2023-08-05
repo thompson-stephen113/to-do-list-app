26eda3eef44e7f783d86bfdee4d9885bb0b2d473
thompson-stephen113 <thompson.stephen113@gmail.com>
2023-08-05 09:21:12 -0400

    converted JS into jQuery in the second step
    Left all old JS as comments and replaced their functionality with jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -21,11 +21,13 @@ function newItem(){
     
     //2. Crossing out an item from the list of items:
     function crossOut() {
-        li.classList.toggle("strike");
+        // li.classList.toggle("strike");
+        li.toggleClass("strike");
     }
     
-        li.addEventListener("dblclick",crossOut);
-    
+    // li.addEventListener("dblclick",crossOut);
+    li.on("click", crossOut)
+
     //3(i). Adding the delete button "X": 
     let crossOutButton = document.createElement("crossOutButton");
         crossOutButton.appendChild(document.createTextNode("X"));
